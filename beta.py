30e1c93ac9ed011daf0ab20354eaa2fc51e48860
Woojin Song <woojinsong93@gmail.com>
2025-03-03 13:30:43 -0800

    Add beta testing file

---
+++ beta.py
@@ -0,0 +1,39 @@
+# File for idea and testing
+
+# Dictionary storing Character's stats
+# character_stats = {'name': 'tester', 'level': 1, 'Health': 100, 'Strength': 15, 'Speed': 10, 'Luck': 5,
+#                   'Honour': 0, 'Ki': 50, 'Spirit': 10, 'Experience': 0, 'Crystals': 0, 'Shards': 10,
+#                   'Current Health': 100, 'X-coordinate': 0, 'Y-coordinate': 0}
+"""
+Health: Life pool. You lose the game if it goes to zero.
+Ki: Resource pool for special abilities.
+Strength: Determines how much physical damage you do.
+Spirit: Determines how much Ki based damage you do.
+Speed: Determines how often it is your turn.
+Luck: Determines your dodge and critical hit chance. Also determines the chance of rare items from merchants.
+Honour: Determines how NPCs interact with you. High honour = favourable options.
+        Negative honour = arrest chance and weaker options.
+Experience: 100 exp needed to level up. Resets back to zero.
+Crystals: Collectable resource needed to battle zone boss.
+Shards: Tradable resource. For upgrading stats and buying items.
+X-coordinate and Y-coordinate: Current character location on the grid.
+"""
+
+
+# Make character including the player's name
+def make_character(player_name):
+    character = {'Name': f'{player_name}', 'Level': 1, 'Health': 100, 'Strength': 15, 'Speed': 10, 'Luck': 5,
+                 'Honour': 0, 'Ki': 50, 'Spirit': 10, 'Experience': 0, 'Crystals': 0, 'Shards': 10,
+                 'Current Health': 100, 'X-coordinate': 0, 'Y-coordinate': 0}
+    return character
+
+
+# Ask user for proper player name
+def proper_name():
+    # Keep asking user for valid name
+    while True:
+        player_name = input("Please enter a valid character name (letters only): ").strip()
+        if player_name.isalpha():
+            return player_name
+        else:
+            print("Not a valid character name. Try again.")
