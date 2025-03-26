162925c3201e611d008612f8a88561a495432975
Woojin Song <woojinsong93@gmail.com>
2025-03-25 22:32:45 -0700

    Create __init__.py and CONSTANT NPC_DICT

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# Store all possible encounter NPCs
+NPC_DICT = {'Friendly': ['Civilian', 'Merchant', 'Guard', 'Fairy'],
+            'Monsters': ['Djinn', 'Skinwalker', 'Ghoul', 'Wendigo', 'Shapeshifter', 'Werewolf', 'Vampire'],
+            'Environment': ['Hot Spring']
+            }
