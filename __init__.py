13e5338fef500a899402e5e8b04ff1c956ef768f
Woojin Song <woojinsong93@gmail.com>
2025-03-29 20:36:31 -0700

    Add BATTLE_STANCE as constant

--- __init__.py
+++ __init__.py
@@ -3,3 +3,9 @@ NPC_DICT = {'Friendly': ['Civilian', 'Merchant', 'Guard', 'Fairy'],
             'Monsters': ['Djinn', 'Skinwalker', 'Ghoul', 'Wendigo', 'Shapeshifter', 'Werewolf', 'Vampire'],
             'Environment': ['Hot Spring']
             }
+# Store character's battle stances. Start with Bear and gain each other one per level.
+BATTLE_STANCES = (
+    ('Bear', "Clavem transforms into a giant greatsword. The edges aren't very sharp but it packs a huge punch."),
+    ('Turtle', "Clavem transforms into a shield. The outer shield can be imbued with ki."),
+    ('Snake', "Clavem transforms into a whip. It can extend to great lengths or split into hundreds of smaller whips.")
+)
