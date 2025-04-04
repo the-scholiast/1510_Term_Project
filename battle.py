b077d0ae27580e68986b99e8279db313f80c8830
Woojin Song <wsong31@my.bcit.ca>
2025-04-03 17:49:08 -0700

    Buff character attack damage

--- battle.py
+++ battle.py
@@ -8,18 +8,18 @@ from __init__ import BATTLE_STANCES
 # Store character's attack moves
 CHARACTER_ATTACKS = {
     'Bear': {
-        'Heavy Strike': ['A powerful blow with massive physical damage.', 'Physical', 15],
-        'Sunder': ['Slams the ground in front of you creating a wave of Ki.', 'Ki', 25],
+        'Heavy Strike': ['A powerful blow with massive physical damage.', 'Physical', 20],
+        'Sunder': ['Slams the ground in front of you creating a wave of Ki.', 'Ki', 35],
         'Berserk': ['Enters a state of rage, increasing both physical damage and Ki attacks.', 'Ki', 0]
     },
     'Turtle': {
-        'Bash': ['Coats the outer shield with spikes, then bashes into the enemy.', 'Physical', 10],
+        'Bash': ['Coats the outer shield with spikes, then bashes into the enemy.', 'Physical', 15],
         'Shell': ['Take no damage next two turns.', 'Ki', 0],
-        'Roar': ["A lion's mouth forms at the center of the shield and shoots a Ki wave.", 'Ki', 40]
+        'Roar': ["A lion's mouth forms at the center of the shield and shoots a Ki wave.", 'Ki', 45]
     },
     'Snake': {
-        'Lash': ['Whip extends to lash its target.', 'Physical', 12],
-        'Snare': ['Whip entangles its target, paralyzing its victim.', 'Ki', 8],
+        'Lash': ['Whip extends to lash its target.', 'Physical', 20],
+        'Snare': ['Whip entangles its target, paralyzing its victim.', 'Ki', 12],
         'Hydra': ['Splits into hundreds of smaller whips making its attack unavoidable.', 'Ki', 50]
     }
 }
